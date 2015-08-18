15ab46fcbfd1973f9455492bfa1c9bc25ffc49af
AhnHyo-Bin <ahnhyo-bin@AhnHyo-Binui-MacBook-Pro.local>
2015-08-18 20:28:21 +0900

    11

---
+++ www/application/views/contact.php
@@ -0,0 +1,39 @@
+
+    <div class="container theme-showcase" role="main"  style=" margin-top: 40px;" >
+      <p class="code"><font class="tag3">class</font> Php_programer <font class="tag1">extends</font> <font class="tag4">Programer</font> {</p>
+
+      <p class="code"><font class="tag1">public</font> <font class="tag3">function</font> <font class="tag4">index</font>()</p>
+      <p class="code">{</p>
+        <p class="code">    $data[<font class="tag2">'name'</font>] <font class="tag1">=</font>  <font class="tag2">"</font><font class="tag2 element">안효빈</font><font class="tag2 ">"</font>;</p>
+        <p class="code">    $data[<font class="tag2">'call'</font>] <font class="tag1">=</font>  <font class="tag2">"010-2552-0560"</font>;</p>
+        <p class="annotation2">   //show_404();</p>
+      <!-- <p class="code">  $this<font class="tag1">-></font>load<font class="tag1">-></font>view(<font class="tag1 element"></font>,$data)</p> -->
+      <p class="code">}</p>
+
+
+    <!-- <span class="typed-cursor smart">|</span> -->
+
+  </div>
+
+  <script src="<?php echo base_url('assets/js/typed.js'); ?>"></script>
+
+  <script>
+    $(function(){
+        $(".element").typed({
+          strings: ["안효빈"],
+          typeSpeed: 300,
+           loop: true,
+        });
+    });
+
+
+window.addEventListener('load', function() {
+setTimeout(scrollTo, 0, 0, 1);
+}, false);
+
+
+  </script>
+    <!--================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+
+
